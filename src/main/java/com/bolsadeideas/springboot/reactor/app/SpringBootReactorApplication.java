7f298d3c8b29cf9eb328ce3436c11636e7346733
Cristian Quiñones <cristian.quinones@sofka.com.co>
2023-05-29 09:44:55 -0500

    Se hacen ejercicios para probar flatmaps

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,13 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+       // ejemploIterable();
+       // ejemploFlatmap();
+        ejemplotoString();
+    }
+
+
+    public void ejemploIterable() throws Exception {
 
         List <String> usuariosList = new ArrayList <>();
         usuariosList.add("Cristian Santamaria");
@@ -34,13 +42,13 @@ public class SpringBootReactorApplication implements CommandLineRunner {
         Flux <String> nombres = Flux.fromIterable(usuariosList);
 
 
-                nombres.map(nombre ->  new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+        Flux <Usuario> usuarios = nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
                 .filter(usuario -> usuario.getNombre().equalsIgnoreCase("bruce"))
-                .doOnNext(usuario ->{
-                    if (usuario == null){
+                .doOnNext(usuario -> {
+                    if (usuario == null) {
                         throw new RuntimeException("Los nombres no pueden estar pelados");
                     }
-                      System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
+                    System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
                 }).map(usuario -> {
                     String nombre = usuario.getNombre().toLowerCase();
                     usuario.setNombre(nombre);
@@ -48,7 +56,7 @@ public class SpringBootReactorApplication implements CommandLineRunner {
                 });
 
 
-        nombres.subscribe(e -> log.info(e.toString()),
+        usuarios.subscribe(e -> log.info(e.toString()),
                 error -> log.error(error.getMessage()),
                 new Runnable() {
                     @Override
@@ -58,4 +66,57 @@ public class SpringBootReactorApplication implements CommandLineRunner {
                 }
         );
     }
+
+    public void ejemploFlatmap() throws Exception {
+
+        List <String> usuariosList = new ArrayList <>();
+        usuariosList.add("Cristian Santamaria");
+        usuariosList.add("Andres Guzman");
+        usuariosList.add("Maria Santamaria");
+        usuariosList.add("Pedro Sarmiento");
+        usuariosList.add("Bruce Lee");
+        usuariosList.add("Bruce Wily");
+
+        Flux.fromIterable(usuariosList)
+                .map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+                .flatMap(usuario -> {
+                    if (usuario.getNombre().equalsIgnoreCase("bruce")){
+                        return Mono.just(usuario);
+                    }else{
+                        return Mono.empty();
+                    }
+                })
+                .map(usuario -> {
+                    String nombre = usuario.getNombre().toLowerCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
+                }).subscribe(u ->log.info(u.toString())
+
+        );
+    }
+
+    public void ejemplotoString() throws Exception {
+
+        List <Usuario> usuariosList = new ArrayList <>();
+        usuariosList.add(new Usuario( "Cristian" ,"Santamaria"));
+        usuariosList.add(new Usuario( "Andres", "Guzman"));
+        usuariosList.add(new Usuario( "Maria", "Santamaria"));
+        usuariosList.add(new Usuario( "Pedro", "Sarmiento"));
+        usuariosList.add(new Usuario( "Bruce", "Lee"));
+        usuariosList.add(new Usuario( "Bruce", "Wily"));
+
+        Flux.fromIterable(usuariosList)
+                .map(usuario -> (usuario.getNombre().toUpperCase().concat(" ").concat(usuario.getApellido().toUpperCase())))
+                .flatMap(nombre -> {
+                    if (nombre.contains("bruce".toUpperCase())){
+                        return Mono.just(nombre);
+                    }else{
+                        return Mono.empty();
+                    }
+                })
+                .map(String::toLowerCase)
+                .subscribe(u ->log.info(u.toString())
+                );
+    }
 }
+

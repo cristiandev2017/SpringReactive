dc99c68854943f98ac5ae8565ed73253c6b3d105
Cristian Quiñones <cristian.quinones@sofka.com.co>
2023-05-29 10:29:05 -0500

    Se unen 2 objetos usando flatmap

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,6 +1,8 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import com.bolsadeideas.springboot.reactor.app.models.Comentarios;
 import com.bolsadeideas.springboot.reactor.app.models.Usuario;
+import com.bolsadeideas.springboot.reactor.app.models.UsuarioComentarios;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -23,10 +25,11 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-       // ejemploIterable();
-       // ejemploFlatmap();
-       //ejemplotoString();
-       ejemplotoCollectList();
+        // ejemploIterable();
+        // ejemploFlatmap();
+        //ejemplotoString();
+        //ejemplotoCollectList();
+        ejemploUsuarioComentariosFlatMap();
     }
 
 
@@ -81,9 +84,9 @@ public class SpringBootReactorApplication implements CommandLineRunner {
         Flux.fromIterable(usuariosList)
                 .map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
                 .flatMap(usuario -> {
-                    if (usuario.getNombre().equalsIgnoreCase("bruce")){
+                    if (usuario.getNombre().equalsIgnoreCase("bruce")) {
                         return Mono.just(usuario);
-                    }else{
+                    } else {
                         return Mono.empty();
                     }
                 })
@@ -91,36 +94,36 @@ public class SpringBootReactorApplication implements CommandLineRunner {
                     String nombre = usuario.getNombre().toLowerCase();
                     usuario.setNombre(nombre);
                     return usuario;
-                }).subscribe(u ->log.info(u.toString())
+                }).subscribe(u -> log.info(u.toString())
 
-        );
+                );
     }
 
     public void ejemplotoString() {
 
         List <Usuario> usuariosList = new ArrayList <>();
-        usuariosList.add(new Usuario( "Cristian" ,"Santamaria"));
-        usuariosList.add(new Usuario( "Andres", "Guzman"));
-        usuariosList.add(new Usuario( "Maria", "Santamaria"));
-        usuariosList.add(new Usuario( "Pedro", "Sarmiento"));
-        usuariosList.add(new Usuario( "Bruce", "Lee"));
-        usuariosList.add(new Usuario( "Bruce", "Wily"));
+        usuariosList.add(new Usuario("Cristian", "Santamaria"));
+        usuariosList.add(new Usuario("Andres", "Guzman"));
+        usuariosList.add(new Usuario("Maria", "Santamaria"));
+        usuariosList.add(new Usuario("Pedro", "Sarmiento"));
+        usuariosList.add(new Usuario("Bruce", "Lee"));
+        usuariosList.add(new Usuario("Bruce", "Wily"));
 
         Flux.fromIterable(usuariosList)
                 .map(usuario -> (usuario.getNombre().toUpperCase().concat(" ").concat(usuario.getApellido().toUpperCase())))
                 .flatMap(nombre -> {
-                    if (nombre.contains("bruce".toUpperCase())){
+                    if (nombre.contains("bruce".toUpperCase())) {
                         return Mono.just(nombre);
-                    }else{
+                    } else {
                         return Mono.empty();
                     }
                 })
                 .map(String::toLowerCase)
-                .subscribe(u ->log.info(u.toString())
+                .subscribe(u -> log.info(u.toString())
                 );
     }
 
-    public void ejemplotoCollectList(){
+    public void ejemplotoCollectList() {
 
         List <Usuario> usuariosList = new ArrayList <>();
         usuariosList.add(new Usuario("Cristian", "Santamaria"));
@@ -134,7 +137,31 @@ public class SpringBootReactorApplication implements CommandLineRunner {
                 .collectList()
                 .subscribe(lista ->
                         lista.forEach(usuario -> log.info(usuario.toString()))
-                        );
+                );
+    }
+
+    public Usuario crearUsuario() {
+        return new Usuario("Jhon", "Wick");
+    }
+
+    public void ejemploUsuarioComentariosFlatMap() {
+        //Forma1 llamando un metodo que me crea el usuario
+        //Mono<Usuario> usuarioMono = Mono.fromCallable(() -> crearUsuario());
+        //Forma 2 (Tambien en vez de el fromCallable se puede usar el Just)
+        Mono <Usuario> usuarioMono = Mono.fromCallable(() -> new Usuario("Jhon", "Wick"));
+
+        Mono <Comentarios> comentariosMono = Mono.fromCallable(() -> {
+            Comentarios comentarios = new Comentarios();
+            comentarios.addComentario("Hola pepe, que tal?");
+            comentarios.addComentario("Por fin terminare el curso");
+            comentarios.addComentario("Con toda muchachos");
+            return comentarios;
+        });
+
+        usuarioMono.flatMap(usuario ->
+                comentariosMono.map(
+                        comentarios -> new UsuarioComentarios(usuario, comentarios))
+        ).subscribe(usuarioComentarios -> log.info(usuarioComentarios.toString()));
     }
 }
 
